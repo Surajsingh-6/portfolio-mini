47e7381e725f200cf7e65446443841452de16442
Suraj singh <suraj142005@gmail.com>
2025-01-28 11:33:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Full Stack Developer ." , "ML Engineer ." , "Data Analyst ."],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1000,
+    loop: true
+})
